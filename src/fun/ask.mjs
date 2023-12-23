f1bc1f48e27f135bb79d347865e56a3c7a2e2fe8
bipproduction <bip.production.js@gmail.com>
2023-12-23 14:34:52 +0800

    tambahan

--- src/fun/ask.mjs
+++ src/fun/ask.mjs
@@ -5,11 +5,13 @@ import ChatBot from "googlebard-js";
 import cok from "./cok.js";
 
 export async function ask(text) {
+    console.log("pertanyaan", text)
     const cookie2 = cok()
     const chatbot = new ChatBot(cookie2);
 
     await chatbot.getAPI();
-
-    return await chatbot.ask(text)
+    const jawaban = await chatbot.ask(text)
+    console.log("jawaban", jawaban)
+    return jawaban
 }
 
--- src/index.js
+++ src/index.js
@@ -11,14 +11,19 @@ const PORT = 3009
 var page;
 var browser;
 
-app.use(express.static(path.join(__dirname, "./public")))
+// app.use(express.static(path.join(__dirname, "./public")))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-//     res.sendFile('./public/index.html')
-// })
+app.get('/', (req, res) => {
+    const url = execSync('hostname').toString().trim() === "bips-MacBook-Air.local"? "http://localhost:3009": "https://surya-ai.wibudev.com"
+
+    // Render halaman HTML dan kirimkan data JSON
+    res.render('index', { "url": url });
+});
 
 
 app.get('/start', async (req, res) => {
@@ -56,7 +61,7 @@ app.get("/screenshot", async (req, res) => {
 
 app.listen(PORT, async () => {
     console.log("start ppt")
-    await start_ppt()
+    // await start_ppt()
     console.log(`server berjalan di port ${PORT}`)
 })
 
